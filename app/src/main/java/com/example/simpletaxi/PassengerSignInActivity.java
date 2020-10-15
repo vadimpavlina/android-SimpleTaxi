e6a4992ec9f029f398fe2e5a1f876852e245db0f
Vadym Pavlina <vadim.table.pc@gmail.com>
2020-10-15 12:11:54 +0300

    first commit

---
+++ app/src/main/java/com/example/simpletaxi/PassengerSignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.simpletaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.simpletaxi.R;
+
+public class PassengerSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_sigin_in);
+    }
+}
